9542dcb59e0990888dd2fd88ebee0b6cc3b92ad7
Serge Dolan <dolan.serge@gmail.com>
2021-01-28 20:26:43 +0000

    Added changeImage.py which converts images form .tiff to .jpeg

---
+++ changeImage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+from PIL import Image
+import re
+
+cwd = os.getcwd()
+path = cwd + '/supplier-data/images/'
+pattern = r".*\.tiff"
+
+for file in os.listdir(path):
+  if re.search(pattern, file) !=None:
+    im = Image.open(path + file)
+    im.convert('RGB').resize((600, 400)).save(path + file.split('.')[0] + '.jpeg', 'JPEG')
+    im.close()
