4c44bde90045b0d9ffa4a9fbd499bab3cacdaf69
Serge Dolan <dolan.serge@gmail.com>
2021-01-31 14:07:10 -0500

    Finished run.py

--- run.py
+++ run.py
@@ -15,24 +15,17 @@ def read_file(file):
     no_lbs = re.search(pattern, description_list[1])
     description_dict['name'] = description_list[0]
     description_dict['weight'] = no_lbs.group(1)
-    description_dict['description'] = description_list[2] 
+    description_dict['description'] = description_list[2]
     description_dict['image'] = file.split(".")[0] + ".jpeg"
   return description_dict
 
 
-for file in file_list:
-  p = read_file(file)
-  print(p) 
-
-
-"""
 def main():
   for file in file_list:
     p = read_file(file)
-    response = requests.post("http://35.193.74.73/feedback", json=p) 
+    response = requests.post("http://35.193.74.73/feedback", json=p)
     print(response.status_code)
 
 
 if __name__ == "__main__":
     main()
-"""
