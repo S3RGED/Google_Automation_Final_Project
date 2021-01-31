0de628476f2716d510948646e199ef6e9513123d
Serge Dolan <dolan.serge@gmail.com>
2021-01-31 14:28:31 -0500

    Reformatted run.py

--- run.py
+++ run.py
@@ -3,28 +3,34 @@ import os
 import requests
 import re
 
+
 path = 'supplier-data/descriptions/'
 file_list = os.listdir(path)
 file_list.sort()
-pattern = r"(\d+)( \w+)"
+
 def read_file(file):
-  with open (path + file) as f:
+
+    pattern = r"(\d+)( \w+)"
     description_list = []
     description_dict = {}
-    description_list = f.read().splitlines()
-    no_lbs = re.search(pattern, description_list[1])
-    description_dict['name'] = description_list[0]
-    description_dict['weight'] = no_lbs.group(1)
-    description_dict['description'] = description_list[2]
-    description_dict['image'] = file.split(".")[0] + ".jpeg"
-  return description_dict
+
+    with open (path + file) as f:
+        description_list = f.read().splitlines()
+        no_lbs = re.search(pattern, description_list[1])
+        description_dict['name'] = description_list[0]
+        description_dict['weight'] = no_lbs.group(1)
+        description_dict['description'] = description_list[2]
+        description_dict['image'] = file.split(".")[0] + ".jpeg"
+
+    return description_dict
 
 
 def main():
-  for file in file_list:
-    p = read_file(file)
-    response = requests.post("http://35.193.74.73/feedback", json=p)
-    print(response.status_code)
+    for file in file_list:
+        p = read_file(file)
+        response = requests.post("http://35.193.74.73/feedback", json=p)
+        print(response.status_code)
+        
 
 
 if __name__ == "__main__":
